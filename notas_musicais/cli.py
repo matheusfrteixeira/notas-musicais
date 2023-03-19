6af13b1b91654f126e3ff5c0fd27d48aae18deb6
Matheus Teixeira <dev.matheusteixeira@gmail.com>
2023-03-19 00:21:16 -0300

    implementação inicial do CLI

---
+++ notas_musicais/cli.py
@@ -0,0 +1,25 @@
+from rich.console import Console
+from rich.table import Table
+from typer import Argument, Typer
+
+from notas_musicais.escalas import escalas
+
+console = Console()
+app = Typer()
+
+
+@app.command()
+def escala(
+    tonica: str = Argument('C', help='Tônica da escala'),
+    tonalidade: str = Argument('maior', help='Tonalidade da escala'),
+):
+    table = Table()
+    notas, graus = escalas(tonica, tonalidade).values()
+
+    for grau in graus:
+
+        table.add_column(grau)
+
+    table.add_row(*notas)
+
+    console.print(table)
